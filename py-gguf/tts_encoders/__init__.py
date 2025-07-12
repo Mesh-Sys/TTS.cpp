57b8f386f9a1d821f1a473bc90379b7646256984
SkyNotion <meshdrew5@gmail.com>
2025-07-12 15:26:18 +0100

    Edits

--- py-gguf/tts_encoders/__init__.py
+++ py-gguf/tts_encoders/__init__.py
@@ -1,6 +1,6 @@
 from .tts_encoder import *
 from .tensor_util import *
-from .parler_tts_gguf_encoder import *
+#from .parler_tts_gguf_encoder import *
 from .t5_encoder_gguf_encoder import *
 from .kokoro_gguf_encoder import *
 from .dia_gguf_encoder import *
